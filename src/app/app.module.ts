1523d375582c845990c20422d4982a80da896120
Cesar Loachamin <cesar@Cesars-MacBook-Pro.local>
2019-05-17 21:00:46 -0500

    use datepicker from ngb and font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import {NgbModule, NgbDatepicker} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SprintListComponent } from './sprint-list/sprint-list.component';
 import { SprintComponent } from './sprint/sprint.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'sprints', component: SprintListComponent},
+  {path: '', redirectTo: '/sprints', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -14,6 +21,8 @@ import { SprintComponent } from './sprint/sprint.component';
     SprintComponent
   ],
   imports: [
+    NgbModule,
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule,
     FormsModule,
